e42943304c923386e633ad12ee874ce6704b134b
Mounajidev <lucilo@gmail.com>
2020-12-27 20:31:27 -0300

    Logica transpaso de color actualizada

---
+++ Assets/Scripts/LevelMagament.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelMagament : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] casillas;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParedesDelMismoColor();
+    }
+
+
+public void ParedesDelMismoColor()
+{
+    player = GameObject.FindGameObjectWithTag("Player");
+    casillas = GameObject.FindGameObjectsWithTag("Casilla");
+    
+    foreach (GameObject casilla in casillas)
+    {
+        if (player.gameObject.GetComponentInChildren<Renderer>().sharedMaterial.color == casilla.gameObject.GetComponent<Renderer>().sharedMaterial.color)
+        {
+            casilla.gameObject.GetComponent<Collider>().isTrigger = true;
+        }
+        else
+        {
+            casilla.gameObject.GetComponent<Collider>().isTrigger = false;
+        }
+    }
+}
+
+}
--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -15,7 +15,8 @@ public class PlayerCtrl : MonoBehaviour
     public float gravityMultiply = 0;
     public float forceJump;
     public RaycastDetection raydect;
-    FormCtrl formControl;// actformT;  
+    FormCtrl formControl;// actformT; 
+    //public Collision paredCol;
 
 
     void Start()
@@ -29,10 +30,12 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+
         Jump();
-       DetectColorThing();//detecta por ahora las cosas que quiera traspasar
+        //DetectColorThing();//detecta por ahora las cosas que quiera traspasar
     }
+
+
     private void FixedUpdate()
     {
 
@@ -41,33 +44,33 @@ public class PlayerCtrl : MonoBehaviour
     }
     private void DetectColorThing()
     {
-            coll=raydect.RetCollBoxDectected();
-        
+        coll = raydect.RetCollBoxDectected();
+
         foreach (Collider c in coll)
-        {   
-            Material mat=this.GetComponentInChildren<Renderer>().sharedMaterial;
+        {
+            Material mat = this.GetComponentInChildren<Renderer>().sharedMaterial;
             c.gameObject.GetComponent<ManagerColorThing>().detectColorThing(mat);
         }
-   
+
     }
     void Move()
     {
         moveH = Input.GetAxis("Horizontal");
-        if (formControl.actformT == FormCtrl.formType.normal)
-        {
+        //if (formControl.actformT == FormCtrl.formType.normal)
+        
             //  rb.transform.position += transform.forward * velocity * moveH * Time.deltaTime;
             Vector3 move = new Vector3(0f, 0f, velocity * moveH * Time.deltaTime);
             rb.MovePosition(transform.position + move);
-            //anim.SetFloat("Horizontal", moveH);
+            anim.SetFloat("PlayerMove", moveH);
 
             //Debug.Log(anim);
-        }
-        else if (formControl.actformT == FormCtrl.formType.sphere)
-        {
-            rb.AddForce(new Vector3(0f, 0f, moveH) * velocityEsphere);
-            rb.AddTorque(new Vector3(moveH, 0f, 0f) * velocityEsphere);
-            rb.AddTorque(transform.right * moveH * velocityEsphere);
-        }
+        
+        //else if (formControl.actformT == FormCtrl.formType.sphere)
+        //{
+        //    rb.AddForce(new Vector3(0f, 0f, moveH) * velocityEsphere);
+        //    rb.AddTorque(new Vector3(moveH, 0f, 0f) * velocityEsphere);
+        //    rb.AddTorque(transform.right * moveH * velocityEsphere);
+        //}
     }
     void Jump()
     {
@@ -81,6 +84,7 @@ public class PlayerCtrl : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Space))//cambiar el input
                 {
                     rb.AddForce(transform.up * forceJump, ForceMode.Impulse);
+                    anim.Play("Jump");
                 }
             }
 
@@ -89,24 +93,80 @@ public class PlayerCtrl : MonoBehaviour
 
     }
 
+
+
+    // ESTE METODO SE TRIGEREA LUEGO DE COLISIONAR LA PRIMERTA VEZ CON LA PRED, LO QUE HACE QUE EN EL PRIMER CONTACTO NO LA ATARAVIECE, LO CUAL NO ES LO QUE BUSCAMOS 
+    // ( ASI QUE ABAJO VAMOS A PROBAR OTRA LOGICA )
+
     public void OnTriggerEnter(Collider other)
     {
-        //cambiar
-        //hay que hacer un contador de tiempo para que le devuelva el color a la esfera que da el color 
-        //la condicion del if debe fijarse si se puede tomar el color o no 
+        //        //cambiar
+        //        //hay que hacer un contador de tiempo para que le devuelva el color a la esfera que da el color 
+        //        //la condicion del if debe fijarse si se puede tomar el color o no 
         if (other.gameObject.layer == Constans.LAYERCOLORSPHERE)
         {
-            this.gameObject.GetComponentInChildren<Renderer>().sharedMaterial = other.gameObject.GetComponent<Renderer>().sharedMaterial;           
+            this.gameObject.GetComponentInChildren<Renderer>().sharedMaterial = other.gameObject.GetComponent<Renderer>().sharedMaterial;
         }
-     
-    }
-   /* public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == Constans.LAYERCOLORTHING)
-        {
-            Material r = this.GetComponentInChildren<Renderer>().sharedMaterial;
-            collision.gameObject.GetComponent<ManagerColorThing>().Desactivar(r);//aca me quede!!!
-        }
-    }*/
 
+    }
 }
+
+
+//    public void OnCollisionEnter(Collision paredCol)
+//    {
+
+//        if (this.GetComponentInChildren<Renderer>().sharedMaterial.color == paredCol.gameObject.GetComponent<Renderer>().sharedMaterial.color)
+//        {
+
+//            paredCol.gameObject.GetComponent<Collider>().isTrigger = true;
+//            Debug.Log("OnCollisionEnter");
+//            //SetTriggerToFalse();
+
+//            //Material r = this.GetComponentInChildren<Renderer>().sharedMaterial;
+//            //collision.gameObject.GetComponent<ManagerColorThing>().Desactivar(r);//aca me quede!!!
+//        }
+//    }
+//    public void OnTriggerExit(Collider paredCol)
+//    {
+//        paredCol.gameObject.GetComponent<Collider>().isTrigger = false;
+//    }
+//}
+
+
+//else if (collision.gameObject.GetComponent<Collider>().isTrigger == true)
+//{
+
+//}
+
+
+
+//}
+////public void OnCollisionExit(Collision paredcol)
+////{
+////    paredCol.gameObject.GetComponent<Collider>().isTrigger = false;
+////}
+////public void SetTriggerToFalse()
+////{
+////    if ( paredCol.gameObject.GetComponent<Collider>().isTrigger == true )
+////    {
+////        Debug.Log("Set Trigger False");
+
+////    }
+
+
+//}
+
+
+//private void OnCollisionExit(Collision collision)
+//{
+//    if (this.GetComponentInChildren<Renderer>().sharedMaterial.color == collision.gameObject.GetComponent<Renderer>().sharedMaterial.color)
+//    {
+//        collision.gameObject.GetComponent<Collider>().isTrigger = false;
+//        Debug.Log("Collision Exit");
+//    }
+//}
+
+
+
+
+//
