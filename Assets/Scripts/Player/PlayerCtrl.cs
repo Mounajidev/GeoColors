ab1cc6d6c7bd94f07051697d86f473287b2e4d13
jefferson perez <jeffersonperezinfo@gmail.com>
2020-12-30 22:40:28 -0300

    ultimoCambio

--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     Animator anim;
-
     public Rigidbody rb;
     public float moveH;
     Collider[] coll;
@@ -14,25 +13,31 @@ public class PlayerCtrl : MonoBehaviour
     public float velocityEsphere = 5f;
     public float gravityMultiply = 0;
     public float forceJump;
+
+    public float dashForce;
+    public float dashDuration;
     public RaycastDetection raydect;
     FormCtrl formControl;// actformT; 
-    //public Collision paredCol;
-
-
+    public bool activedoubleJump, activeDash;
+    Character player;
     void Start()
     {
-
+        player = new Character(this.gameObject);
         rb = GetComponent<Rigidbody>();
         formControl = this.GetComponent<FormCtrl>();
         raydect = GetComponent<RaycastDetection>();
         anim = GetComponent<Animator>();
+
     }
+    ///Hojo hay que cambiar todos los imput para que funcione en android por ahora esta todo como si fuera un teclado
+    ///normal 
+    ///
     // Update is called once per frame
     void Update()
     {
-
-      Jump();
-      DetectColorThing();//detecta por ahora las cosas que quiera traspasar
+        escogerColor();
+        Jump();
+        Dash();
     }
 
 
@@ -41,38 +46,59 @@ public class PlayerCtrl : MonoBehaviour
 
         Move();
         rb.AddForce(-transform.up * gravityMultiply, ForceMode.Acceleration);
+
+
     }
-    private void DetectColorThing()
+    public void escogerColor()
     {
         coll = raydect.RetCollBoxDectected();
-
+        bool chocar = false;
         foreach (Collider c in coll)
         {
-            Material mat = this.GetComponentInChildren<Renderer>().sharedMaterial;
-            c.gameObject.GetComponent<ManagerColorThing>().detectColorThing(mat);
+            if (c.gameObject.GetComponent<Renderer>().sharedMaterial == this.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial)
+            {
+                chocar = true;
+                break;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W) && !chocar)
+        {
+            player.reodenarColor();
+            player.escogerColor();
+            chocar = false;
+        }
+        Debug.Log(chocar);
+    }
+
+    public void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && activeDash == true)
+        {
+            StartCoroutine(Invoke());
         }
 
+    }
+    public IEnumerator Invoke()
+    {
+
+        rb.AddForce(transform.forward * dashForce, ForceMode.VelocityChange);
+        activeDash = false;
+        yield return new WaitForSeconds(dashDuration);
+        rb.velocity = Vector3.zero;
+        activeDash = true;
     }
     void Move()
     {
         moveH = Input.GetAxis("Horizontal");
         //if (formControl.actformT == FormCtrl.formType.normal)
-        
-            //  rb.transform.position += transform.forward * velocity * moveH * Time.deltaTime;
-            Vector3 move = new Vector3(0f, 0f, velocity * moveH * Time.deltaTime);
-            rb.MovePosition(transform.position + move);
+        Vector3 move = new Vector3(0f, 0f, velocity * moveH * Time.deltaTime);
+        rb.MovePosition(transform.position + move);
         if (moveH > 0.1)
-        {
-            anim.SetFloat("PlayerMove", moveH);
-        }
-        //    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        //else if(moveH < -0.1)
-        //     transform.rotation = Quaternion.Euler(0f, -180f, 0f);
-        //anim.SetFloat("Horizontal", moveH);
-        //anim.SetFloat("PlayerMove", moveH);
-
-            //Debug.Log(anim);
-        
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        else if (moveH < -0.1)
+            transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        anim.SetFloat("PlayerMove", moveH);
+        //}
         //else if (formControl.actformT == FormCtrl.formType.sphere)
         //{
         //    rb.AddForce(new Vector3(0f, 0f, moveH) * velocityEsphere);
@@ -92,7 +118,18 @@ public class PlayerCtrl : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Space))//cambiar el input
                 {
                     rb.AddForce(transform.up * forceJump, ForceMode.Impulse);
-                  //  anim.Play("Jump");
+                    //  anim.Play("Jump");
+                }
+            }
+            else if (!detect && activedoubleJump)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))//cambiar el input
+                {
+
+                    float forceJump2 = forceJump + forceJump * 0.1f;
+                    rb.AddForce(transform.up * forceJump2, ForceMode.Impulse);
+                    //  anim.Play("Jump");
+                    activedoubleJump = false;
                 }
             }
 
@@ -101,80 +138,32 @@ public class PlayerCtrl : MonoBehaviour
 
     }
 
-
-
-    // ESTE METODO SE TRIGEREA LUEGO DE COLISIONAR LA PRIMERTA VEZ CON LA PRED, LO QUE HACE QUE EN EL PRIMER CONTACTO NO LA ATARAVIECE, LO CUAL NO ES LO QUE BUSCAMOS 
-    // ( ASI QUE ABAJO VAMOS A PROBAR OTRA LOGICA )
-
     public void OnTriggerEnter(Collider other)
     {
         //        //cambiar
         //        //hay que hacer un contador de tiempo para que le devuelva el color a la esfera que da el color 
         //        //la condicion del if debe fijarse si se puede tomar el color o no 
-        if (other.gameObject.layer == Constans.LAYERCOLORSPHERE)
+
+        bool existe = player.existeColor(other.gameObject.GetComponent<Renderer>().sharedMaterial);
+        if (other.gameObject.layer == Constans.LAYERCOLORSPHERE && !existe)
         {
-            this.gameObject.GetComponentInChildren<Renderer>().sharedMaterial = other.gameObject.GetComponent<Renderer>().sharedMaterial;
+            player.almacenarColores(other.gameObject.GetComponent<Renderer>().sharedMaterial);
+            player.reodenarColor();
+            player.escogerColor();
+            //  this.gameObject.GetComponentInChildren<Renderer>().sharedMaterial = other.gameObject.GetComponent<Renderer>().sharedMaterial;
         }
 
     }
+
+    /* private void DetectColorThing()
+ {
+     coll = raydect.RetCollBoxDectected();
+
+     foreach (Collider c in coll)
+     {
+         Material mat = this.GetComponentInChildren<Renderer>().sharedMaterial;
+         c.gameObject.GetComponent<ManagerColorThing>().detectColorThing(mat);
+     }
+
+ }*/
 }
-
-
-//    public void OnCollisionEnter(Collision paredCol)
-//    {
-
-//        if (this.GetComponentInChildren<Renderer>().sharedMaterial.color == paredCol.gameObject.GetComponent<Renderer>().sharedMaterial.color)
-//        {
-
-//            paredCol.gameObject.GetComponent<Collider>().isTrigger = true;
-//            Debug.Log("OnCollisionEnter");
-//            //SetTriggerToFalse();
-
-//            //Material r = this.GetComponentInChildren<Renderer>().sharedMaterial;
-//            //collision.gameObject.GetComponent<ManagerColorThing>().Desactivar(r);//aca me quede!!!
-//        }
-//    }
-//    public void OnTriggerExit(Collider paredCol)
-//    {
-//        paredCol.gameObject.GetComponent<Collider>().isTrigger = false;
-//    }
-//}
-
-
-//else if (collision.gameObject.GetComponent<Collider>().isTrigger == true)
-//{
-
-//}
-
-
-
-//}
-////public void OnCollisionExit(Collision paredcol)
-////{
-////    paredCol.gameObject.GetComponent<Collider>().isTrigger = false;
-////}
-////public void SetTriggerToFalse()
-////{
-////    if ( paredCol.gameObject.GetComponent<Collider>().isTrigger == true )
-////    {
-////        Debug.Log("Set Trigger False");
-
-////    }
-
-
-//}
-
-
-//private void OnCollisionExit(Collision collision)
-//{
-//    if (this.GetComponentInChildren<Renderer>().sharedMaterial.color == collision.gameObject.GetComponent<Renderer>().sharedMaterial.color)
-//    {
-//        collision.gameObject.GetComponent<Collider>().isTrigger = false;
-//        Debug.Log("Collision Exit");
-//    }
-//}
-
-
-
-
-//
